61efa210ef9acaaeca39c89a9f70e398c73595e0
Max <youneedmax@gmail.com>
2021-11-23 12:59:51 +0100

    change ThemeContext code so the dark mode would be enabled at first

--- src/Components/Atoms/ThemeSwitch.tsx
+++ src/Components/Atoms/ThemeSwitch.tsx
@@ -3,24 +3,24 @@ import { FiSun } from "react-icons/fi";
 import { RiMoonClearLine } from "react-icons/ri";
 
 import "./ThemeSwitch.scss";
-import { ThemeContext } from "../../Contexts/ThemeContext";
+import { Mode, ThemeContext } from "../../Contexts/ThemeContext";
 import Tooltip from "./Tooltip";
 
 const ThemeSwitch: React.FC = () => {
-  const { theme, setMode } = useContext(ThemeContext);
+  const { mode, setMode } = useContext(ThemeContext);
 
   return (
     <Tooltip content="Change theme" position="right" hideOnMobile>
       <div className="theme-switch">
-        {theme === "light" ? (
+        {mode === "light" ? (
           <FiSun
             className="theme-switch__icon"
-            onClick={() => setMode("dark")}
+            onClick={() => setMode(Mode.Dark)}
           />
         ) : (
           <RiMoonClearLine
             className="theme-switch__icon"
-            onClick={() => setMode("light")}
+            onClick={() => setMode(Mode.Light)}
           />
         )}
       </div>
